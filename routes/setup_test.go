1eb39cd082ab7e234b76e58cbb1ee97404da3ac9
Carlos S <carlos.solar@mercadolibre.cl>
2021-05-24 16:39:18 -0400

    add sonar and up coverage.

--- routes/setup_test.go
+++ routes/setup_test.go
@@ -23,6 +23,13 @@ func TestPingRoute(t *testing.T) {
 	assert.Equal(t, "pong", w.Body.String())
 }
 
+type cases struct {
+	input   string
+	amount  int
+	code    int
+	message string
+}
+
 func TestLoanRoute(t *testing.T) {
 	router := Setup()
 
@@ -52,25 +59,49 @@ func TestLoanRoute(t *testing.T) {
 	})
 
 	t.Run("Should be ok on account executive", func(t *testing.T) {
-		body := strings.NewReader(`{"amount": 5}`)
-
-		request, _ := http.NewRequest("POST", fmt.Sprintf("/loan"), body)
-		request.Header.Add("Content-Type", "application/json")
-		request.Header.Add("Secret-Api-Key", "whatever")
-
-		response := httptest.NewRecorder()
-		router.ServeHTTP(response, request)
-
-		loanJson, _ := ioutil.ReadAll(response.Body)
-
-		var loan model.Loan
-		if err := json.Unmarshal([]byte(loanJson), &loan); err != nil {
-			t.Fail()
+		cases := []cases{
+			{
+				input:   `{"amount": 5}`,
+				amount:  5,
+				code:    200,
+				message: "loan ok!",
+			},
+			{
+				input:   `{"amount": 5000}`,
+				amount:  5000,
+				code:    200,
+				message: "loan ok!",
+			},
+			{
+				input:   `{"amount": 50000}`,
+				amount:  50000,
+				code:    200,
+				message: "loan ok!",
+			},
 		}
 
-		assert.Equal(t, http.StatusOK, response.Code)
-		assert.Equal(t, 5, loan.Form.Amount)
-		assert.Equal(t, "loan ok!", loan.Message)
+		for _, c := range cases {
+			t.Run("", func(t *testing.T) {
+				body := strings.NewReader(c.input)
 
+				request, _ := http.NewRequest("POST", fmt.Sprintf("/loan"), body)
+				request.Header.Add("Content-Type", "application/json")
+				request.Header.Add("Secret-Api-Key", "whatever")
+
+				response := httptest.NewRecorder()
+				router.ServeHTTP(response, request)
+
+				loanJson, _ := ioutil.ReadAll(response.Body)
+
+				var loan model.Loan
+				if err := json.Unmarshal([]byte(loanJson), &loan); err != nil {
+					t.Fail()
+				}
+
+				assert.Equal(t, c.code, response.Code)
+				assert.Equal(t, c.amount, loan.Form.Amount)
+				assert.Equal(t, c.message, loan.Message)
+			})
+		}
 	})
 }
